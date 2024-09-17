355f4d4c618489e5c225222f53cd6d69196b8fbf
Hamza Ali <aaly.galy@gmail.com>
2024-09-17 11:18:16 +0500

    Variants tuning Updated

--- Variant 2.0.py
+++ Variant 2.0.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-
 def stepFunction(x):
+    """Step function would be use while training neurons and it is basically a hard limitor function which generate a output one if netsum value from Perceptron class fit funtion is greater than threshold. Thresold value by default we are setting to 0.5"""
     threshold=0.5
     return np.where(x > threshold , 1, 0)
 
 class Perceptron:
-    """This is the class for implementing Perceptron model which is basically derived from supervised Learning"""
+    """This is the class for implementing Perceptron model which is basically derived from supervised Learning learning rate is 0.01 iteration would be 1000 activation function used in this a step function"""
     def __init__(self, learning_rate=0.01, n_iters=1000):
         self.lr = learning_rate
         self.n_iters = n_iters
@@ -16,7 +16,7 @@ class Perceptron:
 
 
     def fit(self, X, y):
-        """In this method you will basically train the training set and assess errors to update weights and bias to acheive target value"""
+        """In this method you will basically train the training set and assess errors to update weights and bias to acheive target value weights will only be updated when error is not equal to zero means it is predicting wrong outputs update bias and weights"""
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
@@ -26,15 +26,15 @@ class Perceptron:
             for idx, x_i in enumerate(X):
                 linear_output = np.dot(x_i, self.weights) + self.bias
                 y_predicted = self.activation_func(linear_output)
-
-                # Perceptron update rule
-                update = self.lr * (y_[idx] - y_predicted)
-                self.weights += update * x_i
-                self.bias += update
+                error=y_[idx] - y_predicted
+                if error!=0:
+                    update = self.lr * (error)
+                    self.weights += update * x_i
+                    self.bias += update
 
 
     def predict(self, X):
-        """In which you are going to predict o\p against the updated weights from fit() functions"""
+        """In which you are going to predict o\\p against the updated weights from fit() functions"""
         linear_output = np.dot(X, self.weights) + self.bias
         y_predicted = self.activation_func(linear_output)
         return y_predicted
